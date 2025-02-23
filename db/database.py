6687c42d0a392fda5a30daba85fd4fd327a8ca55
Lukleinos <sakul.jur@gmail.com>
2025-02-23 15:23:31 +0100

    added get_media function and minor fixes

--- db/database.py
+++ db/database.py
@@ -1,5 +1,5 @@
 import sqlite3
-import streamlit as st
+import streamlit as st # type: ignore
 
 def get_conn():
     conn = sqlite3.connect("db/database.db")
@@ -10,33 +10,37 @@ def db_init():
     conn = get_conn()
     cursor = conn.cursor()
 
-    cursor.execute("""
-        create table if not exists media (
-            id integer primary key autoincrement,
-            title text not null,
-            year integer not null,
-            status text not null,
-            date_watched text,
-            rating integer,
-            unique(title, year)
-        )              
-    """)
+    try:
+        cursor.execute("""
+            create table if not exists media (
+                id integer primary key autoincrement,
+                title text not null,
+                year integer not null,
+                status text not null,
+                date_watched text,
+                rating integer,
+                type tet,
+                unique(title, year)
+            )              
+        """)
 
-    conn.commit()
-    conn.close()
+        conn.commit()
+    except Exception as e:
+        st.session_state["message"] = (f"Error: {e}", "error")
+    finally:
+        conn.close()
 
 
-def insert_media(title, year, status, date_watched, rating):
+def insert_media(title, year, status, date_watched, rating, type):
     conn = get_conn()
     cursor = conn.cursor()
 
     try:
         cursor.execute("""
-            insert into media (title, year, status, date_watched, rating)
-            values (?, ?, ?, ?, ?)""", (title, year, status, date_watched, rating))
+            insert into media (title, year, status, date_watched, rating, type)
+            values (?, ?, ?, ?, ?, ?)""", (title, year, status, date_watched, rating, type))
         
         conn.commit()
-
         st.session_state["message"] = ("Media Added!", "success")
     except sqlite3.IntegrityError:
         st.session_state["message"] = (f"Media already exists or missing information!", "warning")
@@ -46,11 +50,34 @@ def insert_media(title, year, status, date_watched, rating):
         conn.close()
 
 
+def get_media(media_type=None, status=None, order=None):
+
+    conn = get_conn()
+    cursor = conn.cursor()
+
+    try:
+        query = "SELECT * FROM media"
+        params = []
+
+        conditions = []
+        if media_type:
+            conditions.append(f"type = '{media_type}'")
+        if status:
+            conditions.append(f"status = '{status}'")
+
+        if conditions:
+            query += " WHERE " + " AND ".join(conditions)
+        if order:
+            query += f" ORDER BY {order}"
+
+        cursor.execute(query, params)
+        return cursor.fetchall()
+    except Exception as e:
+        st.session_state["message"] = (f"Database error: {e}", "error")
+        return []
+    finally:
+        conn.close()
+
+
 def remove_media():
     pass
-
-
-def get_all_media():
-    pass
-
-
