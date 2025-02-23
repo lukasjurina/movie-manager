93faa035f9d7dbc33accfc60d9b0a8f20cccba73
Lukleinos <sakul.jur@gmail.com>
2025-02-23 15:25:56 +0100

    added get_media funtionality

--- functions/add_media.py
+++ functions/add_media.py
@@ -2,34 +2,40 @@ import streamlit as st # type: ignore
 from datetime import datetime
 from db.database import insert_media
 
-def add_media(media_type):
 
-    @st.dialog(f"Add {media_type}")
-    def add(media_type):
+def add_media(parent_key):
+    """
+    Creates a dialog for adding a movie or show.
+
+    Parameters:
+    parent_key (str): The type of media being added ("Movie" or "Show").
+
+    """
+
+    @st.dialog(f"Add {parent_key}")
+    def add(parent_key):
 
         date = None
         rating = None
 
-        media_title = st.text_input(f"{media_type} Title")
+        media_title = st.text_input(f"{parent_key} Title")
 
         start_year = 1888
         curr_year = datetime.now().year
-
         years = list(range(curr_year, start_year - 1, -1))
         media_year = st.selectbox("Year", years, index = 0)
 
-        if media_type == "Movie":
+        if parent_key == "Movie":
             status_map = {
                 "To Watch",
                 "Watched",
             }
-        if media_type == "Show":
+        if parent_key == "Show":
             status_map = {
                 "To Watch",
                 "Watched",
                 "Watching",
             }
-
         media_status = st.pills("Status", options = status_map, selection_mode = "single")
 
         if media_status == "Watched":
@@ -40,11 +46,9 @@ def add_media(media_type):
 
         if st.button("Submit"):
 
-            insert_media(media_title, media_year, media_status, date, rating)
+            insert_media(media_title, media_year, media_status, date, rating, parent_key)
 
             st.rerun()
-
-
-    if st.button(f"Add {media_type}"):
-        add(media_type)
-        
+    
+    if st.button(f"Add {parent_key}"):
+        add(parent_key)
--- functions/list_media.py
+++ functions/list_media.py
@@ -1,8 +1,8 @@
 import streamlit as st # type: ignore
-
+from db.database import get_media
 
 def filter_media(parent_key):
-    if parent_key == "movies":
+    if parent_key == "Movie":
         status_map = {"To Watch", "Watched"}
     else:
         status_map = {"To Watch", "Watching", "Watched"}
@@ -14,10 +14,12 @@ def filter_media(parent_key):
 
 def order_media(parent_key):
 
-    filter_options = ["Alphabet", "Year", "Date Watched", "Rating"]
-    filter_choice = st.segmented_control("Order By", filter_options, selection_mode = "single", key = f"{parent_key}_filter")
+    filter_map = {"title": "Alphabet", "year": "Year", "date_watched": "Date Watched", "rating": "Rating"}
+    filter_choice = st.segmented_control("Order By", filter_map, format_func = lambda option: filter_map[option], selection_mode = "single", key = f"{parent_key}_filter")
 
     return filter_choice
 
-def list_media(parent_key):
-    pass
+
+def list_media(parent_key, status, order):
+    result = get_media(parent_key, status, order)
+    st.write(result)
